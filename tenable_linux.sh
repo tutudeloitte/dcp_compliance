5004d5da55e2dd76cd3527a96a34a9d3cc425180
tutudeloitte <49918784+tutudeloitte@users.noreply.github.com>
2020-08-25 23:35:10 -0500

    Create tenable_linux.sh

---
+++ tenable_linux.sh
@@ -0,0 +1,11 @@
+# replace [TENABLE-ID] with ID corresponding to your OS
+wget 'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/[TENABLE-ID]/download?i_agree_to_tenable_license_agreement=true' -O /tmp/nesus.rpm
+
+sudo rpm -ivh /tmp/nesus.rpm
+
+# replace [GROUP-NAME] with group name corresponding to AWS account deployed into
+sudo /opt/nessus_agent/sbin/nessuscli agent link --host=cloud.tenable.com --port=443 --key=a521b5ff16a5d5272109d675bba8d84bd07e7126d686c1966ec8e1fce13abd16 --groups=[GROUP-NAME]
+
+sudo /sbin/service nessusagent start
+sudo systemctl status nessusagent.service 
+sudo systemctl start nessusagent.service
